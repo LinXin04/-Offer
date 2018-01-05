874861707bfad1451ef478c861a9ec0318ae8d88
LinXin04 <1647399840@qq.com>
2018-01-05 15:29:19 +0800

    丑数
    把只包含因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含因子7。
    习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。

---
+++ 33.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int GetUglyNumber_Solution(int index) {
+        if(index<=0) return 0;
+        
+        int result[]=new int[index];
+        result[0]=1;
+        int t2=0,t3=0,t5=0;
+        for(int i=1;i<index;i++)
+        {
+            result[i]=Math.min(result[t2]*2,Math.min(result[t3]*3,result[t5]*5));
+            if(result[i]==result[t2]*2) t2++;
+            if(result[i]==result[t3]*3) t3++;
+            if(result[i]==result[t5]*5) t5++;
+        }
+        return result[index-1];
+    }
+}
